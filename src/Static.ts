25f267ba3a3e068fe8ab94763b4dc7c65cc1f106
Ilia Petrovskii <ipetrovskii1301@gmail.com>
2022-10-25 15:00:06 +0300

    Модификаторы доступа

--- src/Static.ts
+++ src/Static.ts
@@ -4,4 +4,15 @@
 3. Добавьте публичный статический метод `PI`, возвращающий значение `Math.PI`
 4. Добавьте публичный статический метод `ceil`, возвращающий число округленное в большую сторону
  */
-export default class TestClass {}
+export default class TestClass {
+    public counter: number = 0;
+    public static counterStatic: number = 0;
+
+    static PI(): number {
+        return Math.PI;
+    }
+
+    static ceil(num: number): number {
+        return Math.ceil(num);
+    }
+}
